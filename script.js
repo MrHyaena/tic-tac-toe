edc933cc45f8fc9ff32df6e9c0d3f2976216b186
MrHyaena <dolezalmartin131@gmail.com>
2024-10-21 19:18:25 +0200

    I put everything into createGame function

--- script.js
+++ script.js
@@ -1,62 +1,109 @@
-function createGame(name) {
-  return (gameboard = {
-    name: name,
-    turn: 0,
-    a1: "",
-    a2: "",
-    a3: "",
-    b1: "",
-    b2: "",
-    b3: "",
-    c1: "",
-    c2: "",
-    c3: "",
-  });
-}
+let myGame = createGame();
 
-function createPlayer(playerSymbol) {
+function createGame() {
   return {
-    marker: playerSymbol,
-    turn(field) {
-      gameboard[field] = this.marker;
-      if (gameboard.turn == 0) {
-        gameboard.turn = 1;
-      } else {
-        gameboard.turn = 0;
-      }
-      console.log(gameboard);
-    },
+    gameboard: createBoard(),
+    playerOne: createPlayer("X"),
+    playerTwo: createPlayer("O"),
   };
-}
 
-let game = createGame("MyGame");
-let playerOne = createPlayer("X");
-let playerTwo = createPlayer("O");
+  // Function for creating board object with names
 
-function checkWinner() {
-  if (
-    (gameboard.a1 === "O") & (gameboard.a2 === "O") & (gameboard.a3 === "O") ||
-    (gameboard.b1 === "O") & (gameboard.b2 === "O") & (gameboard.b3 === "O") ||
-    (gameboard.c1 === "O") & (gameboard.c2 === "O") & (gameboard.c3 === "O") ||
-    (gameboard.a1 === "O") & (gameboard.b1 === "O") & (gameboard.c1 === "O") ||
-    (gameboard.a2 === "O") & (gameboard.b2 === "O") & (gameboard.c2 === "O") ||
-    (gameboard.a3 === "O") & (gameboard.b3 === "O") & (gameboard.c3 === "O") ||
-    (gameboard.a1 === "O") & (gameboard.b2 === "O") & (gameboard.c3 === "O") ||
-    (gameboard.c1 === "O") & (gameboard.b2 === "O") & (gameboard.a3 === "O")
-  ) {
-    console.log("Player Two is winner!");
-  } else if (
-    (gameboard.a1 === "X") & (gameboard.a2 === "X") & (gameboard.a3 === "X") ||
-    (gameboard.b1 === "X") & (gameboard.b2 === "X") & (gameboard.b3 === "X") ||
-    (gameboard.c1 === "X") & (gameboard.c2 === "X") & (gameboard.c3 === "X") ||
-    (gameboard.a1 === "X") & (gameboard.b1 === "X") & (gameboard.c1 === "X") ||
-    (gameboard.a2 === "X") & (gameboard.b2 === "X") & (gameboard.c2 === "X") ||
-    (gameboard.a3 === "X") & (gameboard.b3 === "X") & (gameboard.c3 === "X") ||
-    (gameboard.a1 === "X") & (gameboard.b2 === "X") & (gameboard.c3 === "X") ||
-    (gameboard.c1 === "X") & (gameboard.b2 === "X") & (gameboard.a3 === "X")
-  ) {
-    console.log("Player One is winner!");
-  } else {
-    console.log("Next Player Turn");
+  function createBoard() {
+    return {
+      turn: 0,
+      a1: "",
+      a2: "",
+      a3: "",
+      b1: "",
+      b2: "",
+      b3: "",
+      c1: "",
+      c2: "",
+      c3: "",
+    };
+  }
+
+  // Creates a player object with symbol and function
+  // for playing one turn
+
+  function createPlayer(playerSymbol) {
+    return {
+      marker: playerSymbol,
+      turn(field) {
+        myGame.gameboard[field] = this.marker;
+        if (myGame.gameboard.turn == 0) {
+          myGame.gameboard.turn = 1;
+          checkWinner();
+        } else {
+          myGame.gameboard.turn = 0;
+          checkWinner();
+        }
+        console.log(myGame.gameboard);
+      },
+    };
+  }
+
+  // This is function for checking the winner
+  // based on rows, columns and diagonal
+
+  function checkWinner() {
+    if (
+      (myGame.gameboard.a1 === "O") &
+        (myGame.gameboard.a2 === "O") &
+        (myGame.gameboard.a3 === "O") ||
+      (myGame.gameboard.b1 === "O") &
+        (myGame.gameboard.b2 === "O") &
+        (myGame.gameboard.b3 === "O") ||
+      (myGame.gameboard.c1 === "O") &
+        (myGame.gameboard.c2 === "O") &
+        (myGame.gameboard.c3 === "O") ||
+      (myGame.gameboard.a1 === "O") &
+        (myGame.gameboard.b1 === "O") &
+        (myGame.gameboard.c1 === "O") ||
+      (myGame.gameboard.a2 === "O") &
+        (myGame.gameboard.b2 === "O") &
+        (myGame.gameboard.c2 === "O") ||
+      (myGame.gameboard.a3 === "O") &
+        (myGame.gameboard.b3 === "O") &
+        (myGame.gameboard.c3 === "O") ||
+      (myGame.gameboard.a1 === "O") &
+        (myGame.gameboard.b2 === "O") &
+        (myGame.gameboard.c3 === "O") ||
+      (myGame.gameboard.c1 === "O") &
+        (myGame.gameboard.b2 === "O") &
+        (myGame.gameboard.a3 === "O")
+    ) {
+      console.log("Player Two is winner!");
+    } else if (
+      (myGame.gameboard.a1 === "X") &
+        (myGame.gameboard.a2 === "X") &
+        (myGame.gameboard.a3 === "X") ||
+      (myGame.gameboard.b1 === "X") &
+        (myGame.gameboard.b2 === "X") &
+        (myGame.gameboard.b3 === "X") ||
+      (myGame.gameboard.c1 === "X") &
+        (myGame.gameboard.c2 === "X") &
+        (myGame.gameboard.c3 === "X") ||
+      (myGame.gameboard.a1 === "X") &
+        (myGame.gameboard.b1 === "X") &
+        (myGame.gameboard.c1 === "X") ||
+      (myGame.gameboard.a2 === "X") &
+        (myGame.gameboard.b2 === "X") &
+        (myGame.gameboard.c2 === "X") ||
+      (myGame.gameboard.a3 === "X") &
+        (myGame.gameboard.b3 === "X") &
+        (myGame.gameboard.c3 === "X") ||
+      (myGame.gameboard.a1 === "X") &
+        (myGame.gameboard.b2 === "X") &
+        (myGame.gameboard.c3 === "X") ||
+      (myGame.gameboard.c1 === "X") &
+        (myGame.gameboard.b2 === "X") &
+        (myGame.gameboard.a3 === "X")
+    ) {
+      console.log("Player One is winner!");
+    } else {
+      console.log("Next Player Turn");
+    }
   }
 }
